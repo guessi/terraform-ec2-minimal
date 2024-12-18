23395237a7f3da49309960575b747b7cd5ba908d
guessi <guessi@gmail.com>
2024-12-18 16:56:49 +0800

    Support for Instance Connect

--- main.tf
+++ main.tf
@@ -17,6 +17,41 @@ data "aws_ami" "default" {
   owners = ["099720109477"] # Canonical
 }
 
+// IAM setup for Instance Connect
+
+resource "aws_iam_instance_profile" "default" {
+  name = "instance-profile-demo"
+  role = aws_iam_role.default.name
+}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy" "AmazonSSMManagedInstanceCore" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
+  role       = aws_iam_role.default.name
+  policy_arn = data.aws_iam_policy.AmazonSSMManagedInstanceCore.arn
+}
+
+resource "aws_iam_role" "default" {
+  name               = "instance-profile-role-demo"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.default.json
+}
+
 // render boot up scripts with `cloudinit_config`
 // - ref: https://registry.terraform.io/providers/hashicorp/cloudinit/latest/docs/data-sources/config
 
@@ -30,6 +65,13 @@ data "cloudinit_config" "default" {
 
     content = <<-EOF
     #!/bin/bash
+
+    # https://docs.aws.amazon.com/systems-manager/latest/userguide/agent-install-ubuntu-64-snap.html
+    sudo snap install amazon-ssm-agent --classic
+    sudo snap list amazon-ssm-agent
+    sudo snap start amazon-ssm-agent
+    sudo snap services amazon-ssm-agent
+
     sudo apt update
     sudo apt install -y apache2
     EOF
@@ -40,10 +82,11 @@ data "cloudinit_config" "default" {
 // - ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
 resource "aws_instance" "default" {
-  instance_type    = var.instance_type
-  key_name         = var.key_name
-  ami              = data.aws_ami.default.id
-  user_data_base64 = data.cloudinit_config.default.rendered
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  ami                         = data.aws_ami.default.id
+  associate_public_ip_address = true
+  user_data_base64            = data.cloudinit_config.default.rendered
 
   root_block_device {
     volume_size = var.volume_size
@@ -59,4 +102,6 @@ resource "aws_instance" "default" {
     http_tokens                 = "required"
     instance_metadata_tags      = "disabled"
   }
+
+  iam_instance_profile = aws_iam_instance_profile.default.id
 }

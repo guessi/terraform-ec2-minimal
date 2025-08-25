1c77b97ceeb3a476a9ae99421b8314998af250fa
guessi <guessi@gmail.com>
2025-08-25 12:38:17 +0800

    Bump Terraform 1.12+ and dependencies

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Minimal setup for creating EC2 instance with customized boot up script
 
 - AWS IAM User/Role with EC2 Full Access Permission
 - Proper configured AWS profile
-- Terraform >= 1.10
+- Terraform >= 1.12
 
 ### Setup Guide
 
--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
   # https://support.hashicorp.com/hc/en-us/articles/360021185113-Support-Period-and-End-of-Life-EOL-Policy
   # https://endoflife.date/terraform
-  required_version = ">= 1.10.5"
+  required_version = ">= 1.12.0"
 
   required_providers {
     # https://registry.terraform.io/providers/hashicorp/aws/latest
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.96"
+      version = "~> 6.10"
     }
   }
 }

5019538ee470b23425dd7e6823b09a4f05ca19eb
guessi <guessi@gmail.com>
2025-12-17 16:13:49 +0800

    Bump Terraform 1.13+ and dependencies

--- versions.tf
+++ versions.tf
@@ -1,14 +1,16 @@
 terraform {
   # https://support.hashicorp.com/hc/en-us/articles/360021185113-Support-Period-and-End-of-Life-EOL-Policy
   # https://endoflife.date/terraform
-  required_version = ">= 1.12.0"
+  required_version = ">= 1.13.0"
 
   required_providers {
     # https://registry.terraform.io/providers/hashicorp/aws/latest
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.10"
+      version = "~> 6.26"
     }
+
+    # https://registry.terraform.io/providers/hashicorp/random/latest
     random = {
       source  = "hashicorp/random"
       version = "~> 3.7"
